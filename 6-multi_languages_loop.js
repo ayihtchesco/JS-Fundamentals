f2281ec1f966e3cd4bbe80d04ebbb2f384141a45
Tchesco Ayih <atchesco@gmail.com>
2025-05-20 10:39:52 +0000

    While loop test

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i] + (i < lines.length - 1 ? '\n' : '');
+}
+
+console.log(output);
